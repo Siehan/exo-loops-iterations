d077cd55c4d2be2b9b22225e419e361aaa4efe26
Siehan <memain.ye.sylvie@gmail.com>
2021-02-27 11:29:03 +0100

    done

---
+++ loop5.js
@@ -0,0 +1,18 @@
+/*
+loop5.js
+
+Avec la méthode forEach afficher les éléments du tableau suivant, ligne par ligne :
+let tab = ['map', 'filter', 'forEach', 'reduce', 'every']
+*/
+
+let tab = ["map", "filter", "forEach", "reduce", "every"];
+tab.forEach((element) => console.log(element));
+
+/*
+OUTPOOT:
+map
+filter
+forEach
+reduce
+every
+*/
