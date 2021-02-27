e04144587da0f81425fd88e2b86bb8a18ce0f32b
Siehan <memain.ye.sylvie@gmail.com>
2021-02-27 10:49:45 +0100

    done

---
+++ loop4.js
@@ -0,0 +1,24 @@
+/*
+loop4.js
+
+Avec une boucle for-of afficher les éléments du tableau suivant, ligne par ligne:
+let tab = ['Js', 'Solidity', 'Html', 'Css', 'Python', 'Java', 'C++', 'Rust']
+
+*/
+
+let tab = ["Js", "Solidity", "Html", "Css", "Python", "Java", "C++", "Rust"];
+for (const elem of tab) {
+  console.log(elem);
+}
+
+/*
+OUTPOOT:
+Js
+Solidity
+Html
+Css
+Python
+Java
+C++
+Rust
+*/
